714891b8cf17276b701d6568a9ed8378abfe6703
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2024-09-03 20:09:36 +0900

    test commit

--- codegarden/src/main/java/com/ureca/CodegardenApplication.java
+++ codegarden/src/main/java/com/ureca/CodegardenApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodeGardenBeApplication {
+public class CodegardenApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CodeGardenBeApplication.class, args);
-		//test commit
+		SpringApplication.run(CodegardenApplication.class, args);
 	}
 
 }
